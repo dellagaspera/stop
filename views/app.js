50c40352185a43bf2ac0b458fa7aaaf1425933e5
Gabriel Della Gaspera <gabriel.gaspera@gmail.com>
2026-06-23 16:50:02 -0300

    pare

---
+++ views/app.js
@@ -0,0 +1,106 @@
+const express = require('express');
+const session = require('express-session');
+const http = require('http');
+const bcrypt = require('bcryptjs');
+const { Server } = require('socket.io');
+const { User } = require('./db');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+app.use(session({
+  secret: 'segredo_super_seguro_do_stop',
+  resave: false,
+  saveUninitialized: false
+}));
+
+app.get('/login', (req, res) => res.render('login'));
+
+app.post('/login', async (req, res) => {
+  const { username, password } = req.body;
+  const user = await User.findOne({ where: { username } });
+  
+  if (user) {
+    const isValid = bcrypt.compare(password, user.password);
+    
+    if (isValid) {
+      req.session.userId = user.id;
+      req.session.username = user.username;
+      return res.redirect('/lobby');
+    }
+  }
+  res.send('Usuário ou senha inválidos');
+});
+
+app.post('/cadastro', async (req, res) => {
+  const { username, password } = req.body;
+  let user = await User.findOne({ where: { username } });
+  
+  if (user) {
+    res.send('Usuário já existe');
+  }
+  else {
+    await User.create({
+      username,
+      password // o hash é feito no db.js
+    });
+    user = await User.findOne({ where: { username } });
+    req.session.userId = user.id;
+    req.session.username = user.username;
+    return res.redirect('/lobby');
+  }
+});
+
+function requireAuth(req, res, next) {
+    if(req.session.userId) next();
+    else res.redirect('/login');
+}
+
+app.get('/lobby', requireAuth, (req, res) => {
+  res.render('lobby', { username: req.session.username });
+});
+
+app.get('/jogo', requireAuth, (req, res) => {
+  const sala = req.query.sala;
+  res.render('jogo', { username: req.session.username, sala });
+});
+
+
+
+
+
+
+
+io.on('connection', (socket) => {
+  console.log('Novo jogador conectado:', socket.id);
+
+  socket.on('entrar_sala', (dados) => {
+    const { sala, username } = dados;
+    socket.join(sala);
+    
+    // Avisa apenas os usuários DENTRO da sala que alguém entrou
+    // [PREENCHA AQUI: Use io.to().emit() para enviar o evento 'mensagem_chat' para a sala]
+  });
+
+  socket.on('sortear_letra', (sala) => {
+    const alfabeto = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const letraSorteada = alfabeto[Math.floor(Math.random() * alfabeto.length)];
+    
+    // Dispara a letra para todos da sala
+    io.to(sala).emit('letra_sorteada', letraSorteada);
+  });
+
+  // DESAFIO 3: O Botão STOP!
+  socket.on('gritar_stop', (dados) => {
+    const { sala, username } = dados;
+    // O evento abaixo deve avisar todos na sala para travarem seus inputs e enviarem as respostas.
+    // [PREENCHA AQUI: Emita o evento 'fim_de_rodada' contendo o nome de quem gritou STOP]
+  });
+});
+
+server.listen(3000, () => console.log('Servidor rodando na porta 3000'));
